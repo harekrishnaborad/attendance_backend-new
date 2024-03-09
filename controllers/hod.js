0400182e1e95eb975bc2c7f92392aa3cf5f6790e
harekrishna <hrborad1796@gmail.com>
2024-03-09 16:36:42 +0530

    hk laptop done setup

---
+++ controllers/hod.js
@@ -0,0 +1,55 @@
+const dbs = require('../models/dbs')
+
+// let attendance = 'table_5'
+// let users = 'users'
+let subjects = 'subjects'
+
+
+module.exports = {
+    
+
+    getHome: (request,response)=>{
+        response.render('hod_home.ejs', {user_id: request.session.user_id})
+    },
+
+    create_subject: (request,response)=>{
+        response.render('create_subjects.ejs', { message: "data" ,user_id: request.session.user_id,})
+    },
+
+    update_subject: (request,response)=>{
+        response.render('update_subject.ejs', { message: "data" ,user_id: request.session.user_id,})
+    },
+
+    delete_subject: (request,response)=>{
+        response.render('delete_subject.ejs', { message: "data" ,user_id: request.session.user_id,})
+    },
+
+    show_subject: async (request, response) => {
+        let data = await dbs.queries([`select * from ${subjects};`])
+        console.log(data)
+        response.render('show_subjects.ejs', { result_subjects: data[0] ,user_id: request.session.user_id,})
+    },
+
+    createSubject: async (request, response) => {
+        let data = await dbs.queries([`insert into ${subjects} values('${request.body.subject_name}');`, `select distinct * from users;`])
+        response.render('subject_created.ejs', { subject_name: request.body.subject_name ,user_id: request.session.user_id,})
+    },
+
+    updateSubject: async (request, response) => {
+        let data = await dbs.queries([`update ${subjects} set name = '${request.body.new_name}' where name = '${request.body.previous_name}';`, 'select * from subjects'])
+        response.render('show_subjects.ejs', { result_subjects: data[1][0] ,user_id: request.session.user_id,})
+    },
+
+    deleteSubject: async (request, response) => {
+        let data = await dbs.queries([`delete from ${subjects} where name = '${request.body.subject_name}';`, 'select * from subjects', `select distinct * from users;`])
+        response.redirect("/show_subject")
+    },
+    logout: (request, response) => {
+        request.session.destroy((err) => {
+            if(err) throw err;
+            response.redirect('/')
+        })
+    }
+}
+
+
---
+++ controllers/login.js
@@ -0,0 +1,84 @@
+const dbs = require('../models/dbs')
+const bcrypt = require('bcryptjs')
+
+// let attendance = 'table_5'
+let users = 'user'
+// let subjects = 'subjects'
+
+module.exports = {
+    getBuilding: (req,res)=>{
+        res.render('buildings.ejs')
+    },
+
+
+    getRegister: (req,res) => {
+        res.render('register.ejs', {info: "noError"})
+    },
+
+    addUser: async (req, res) => {
+        const {username, email, password} = req.body
+        console.log({username, email, password})
+
+        let user = await dbs.queries([`select * from ${users} where email = '${email}'`])
+        console.log(user[0][0])
+        if (user[0][0] !== undefined) {
+            if(user[0][0].email == email){
+                return res.redirect('/')
+            }
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+        await dbs.queries([`insert into ${users} values('${username}', '${email}', '${hashedPassword}', '${req.body.role}');`])
+
+        // res.redirect('/login')
+        res.redirect("/login")
+
+
+
+        // await dbs.queries([`insert into ${users} values('${req.body.username}', '${req.body.email}', '${req.body.password}', '${req.body.role}');`])
+    },
+
+
+    getLogin: (req,res) => {
+        res.render('login.ejs', { info: "noError" })
+    },
+
+    checkUsers: async(req,res) => {
+        const {email, password} = req.body
+        let user = await dbs.queries([`select * from ${users} where email = '${email}'`])
+        console.log(user[0][0])
+
+        if(user[0][0] == undefined){
+            return res.render('/login', {info: "error"})
+        }
+        
+        const isMatch = await bcrypt.compare(password, user[0][0].password)
+        console.log(isMatch)
+
+        if(!isMatch){
+            return res.render('/login', {info: "error"})
+        }
+
+        req.session.isAuth = true;
+        req.session.user_id = {
+            "user_name": user[0][0].user_name,
+            "role": user[0][0].role
+        }
+
+        res.redirect('/redirect')
+        // if (req.body.role == "hod") {
+        //     res.redirect("/hod")
+        // }
+        // else if(req.body.role == "faculty"){
+        //     res.redirect("/faculty")
+        // }
+    },
+    redirect_user: (req, res) => {
+        if (req.session.user_id.role == "hod") {
+            res.redirect("/hod")
+        }
+        else if(req.session.user_id.role == "faculty"){
+            res.redirect("/faculty")
+        }
+    }
+}
